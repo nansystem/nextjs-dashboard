011b69954544e411a3a6b114880e93d97d2c3744
nansystem <sato171786@gmail.com>
2025-02-15 12:52:39 +0900

    feat: add suspence

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,12 +13,11 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -29,6 +28,8 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
